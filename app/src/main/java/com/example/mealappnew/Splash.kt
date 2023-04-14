f8be1332a4331d50abea37e8813d5c4061e0b17d
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-04-14 23:38:50 +0530

    Splash updated

--- app/src/main/java/com/example/mealappnew/Splash.kt
+++ app/src/main/java/com/example/mealappnew/Splash.kt
@@ -18,7 +18,7 @@ class Splash : AppCompatActivity() {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
             finish()
-        },4000)
+        },3000)
 
     }
 }
